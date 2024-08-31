82ecc4436ef3e1c6fcbd864ce2743c4ff9d1dd15
Sadiq Kabiru <webemail655@gmail.com>
2024-09-01 00:07:57 +0100

    Manage User

---
+++ BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Bulky.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    //[Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ApplicationUser> objUserList = _context.ApplicationUsers.Include(u => u.Company).ToList();
+            foreach (var user in objUserList)
+            {
+                if (user.Company == null)
+                {
+                    user.Company = new() { Name = "" };
+                }
+            }
+            return Json(new { data = objUserList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            //var CompanyToBeDeleted = _unitOfWork.Company.Get(u => u.Id == id);
+            //if (CompanyToBeDeleted == null)
+            //{
+            //    return Json(new { success  = false, message = "Error while deleting"});
+            //}
+
+            //_unitOfWork.Company.Remove(CompanyToBeDeleted);
+            //_unitOfWork.Save();
+
+            return Json(new { success = true, message = "Deleted Successfully!!!" });
+        }
+
+        #endregion
+    }
+}
--- BulkyWeb/wwwroot/js/company.js
+++ BulkyWeb/wwwroot/js/company.js
@@ -5,21 +5,20 @@ $(document).ready(function () {
 
 function loadDataTable(){
     dataTable = $('#tblData').DataTable({
-        "ajax": { url: '/admin/company/getall' },
+        "ajax": { url: '/admin/user/getall' },
         "columns": [
             //{ data: 'id', "width": "15%" },
             { data: 'name', "width": "25%" },
-            { data: 'streetAddress', "width": "25%" }, 
-            { data: 'city', "width": "15%" },
-            { data: 'state', "width": "15%" },
-            { data: 'postalCode', "width": "10%" },
+            { data: 'email', "width": "25%" }, 
+            { data: 'phoneNumber', "width": "15%" },
+            { data: 'company.name', "width": "15%" },
+            { data: '', "width": "10%" },
 
             {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                               <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2" <i class="bi bi-pencil-square"></i>Edit </a>
-                               <a onClick=Delete('/admin/company/delete/${data}') class="btn btn-danger mx-2" <i class="bi bi-trash-fil"></i>Delete </a>
+                               <a href="/admin/company/upsert?id=${data}" class="btn btn-primary mx-2" <i class="bi bi-pencil-square"></i>Edit </a>    
                             </div>`
                 }, 
                 "width": "15%"
@@ -27,27 +26,6 @@ function loadDataTable(){
         ]
     });
 }
-function Delete(url) {
-    Swal.fire({
-        title: "Are you sure?",
-        text: "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Yes, delete it!"
-    }).then((result) => {
-        if (result.isConfirmed) {
-            $.ajax({
-                url: url,
-                type: 'DELETE',
-                success: function (data) {
-                    dataTable.ajax.reload();
-                    toastr.success(data.message);
-                }
-            })
-        }
-    });
-}
+
 
  
